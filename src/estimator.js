34f3b888be02bf60e5f941b302054a3c26f22495
Damilola Alabi <47612686+Dame01droid@users.noreply.github.com>
2020-04-09 21:56:51 +0100

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,17 @@
 const covid19ImpactEstimator = (data) => data;
 
 export default covid19ImpactEstimator;
+
+"data"; {
+    "region":   {
+        "name":"Africa",
+        "avgAge": 19.7,
+        "AvgDailyIncomeInUSD": 4,
+        "avgDailyIncomePopulation"; 0.73
+    },
+    "periodType":  "days",
+    "timeToELapse": 38,
+    "reportedCases": 2747,
+    "population": 92931687,
+    "totalHospitalBeds": 678874
+},
