e7f2f3508f815690a5493093b70a952a73917412
Damilola Alabi <47612686+Dame01droid@users.noreply.github.com>
2020-04-10 02:39:42 +0100

    Estimator Challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,29 +1,59 @@
-const covid19ImpactEstimator = (data) => data;
-function currentlyInfectedPeople(reportedCases, times) {
-    return reportedCases *times;
+estimator();
 
-}
-function infectionsRequested(currentlyInfected, period) {
-    return currentlyInfected * 2 ** (perriod / 3);
-
-}
-const covid19ImpactEstimator = (data) => {
+function estimator() {
+  function currentlyInfectedPeople(reportedCases, times) {
+    return reportedCases * times;
+  }
+  function infectionsRequested(currentlyInfected, period, type) {
+    let infectionsByRequestedTime;
+    switch (type) {
+      case 'weeks':
+        infectionsRequestedTime =
+          currentlyInfected * 2 ** Math.floor((period * 7) / 3);
+        break;
+      case 'months':
+        infectionsByrequestedTime = currentlyInfected * 2 ** (period * 10);
+        break;
+      default:
+        infectionsByrequestedTime =
+          currentylyInfected * 2 ** Math.ceil(period / 3);
+    }
+    return infectionsByRequestedTime;
+  }
+  const covid19ImpactEstimator = (data) => {
     const input = data;
-    const currentlyInfectedImpact =currrentlyInfectedPeople(data.reportedCases, times: 10);
-    const currentlyInfectedSevere =currentlyInfectedPeople(data.reportedCases, times: 50);
-    return {
+    const currentlyInfectedImpact = currrentlyInfectedPeople(
+      data.reportedCases,
+      times,
+      10
+    );
+    const currentlyInfectedSevere = currentlyInfectedPeople(
+      data.reportedCases,
+      times,
+      50
+    );
+    return (
+      {
         data: input,
         impact: {
-            currentlyInfected: currentlyInfectedImpact,
-            infectionsByrequetedTime: infectionsRequested(currentlyInfectedImpact, dtaa.timeToElapse)
+          currentlyInfected: currentlyInfectedImpact,
+          infectionsByrequestedTime: infectionsRequested(
+            currentlyInfectedImpact,
+            data.timeToElapse,
+            data.periodType
+          )
         }
-    },
-    severeImpact: {
-        currentlyInfected: currentlyInfectedSevere,
-        infectionsByRequestedTime: infectionsRequested(currentlyInfectedSevere, data.timeToElapse)
-        
+      },
+      severeImpact
+    );
+    {
+      currentlyInfected: currentlyInfectedSevere, infectionsByRequestedTime;
+      infectionsRequested(
+        currentlyInfectedSevere,
+        data.timeToElapse,
+        data.periodType
+      );
     }
-
+  };
+  export default covid19ImpactEstimator;
 }
-export default covid19ImpactEstimator;
-
