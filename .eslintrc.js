7bc277d6713d7fc6d4d81d78244df58316f3e3a2
Damilola Alabi <47612686+Dame01droid@users.noreply.github.com>
2020-04-13 22:26:15 +0100

    changed formating rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,23 @@
-export const env = {
-  browser: true,
-  es6: true,
-};
-export const extends = [
-  'airbnb-base',
-];
-export const globals = {
-  Atomics: 'readonly',
-  SharedArrayBuffer: 'readonly',
-};
-export const parserOptions = {
-  ecmaVersion: 2018,
-  sourceType: 'module',
-};
-export const rules = {};
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "comma-dangle": ["error", "never"],
+    "no-param-reassign": ["error", { "props": false }]
+  },
+}; 
--- src/estimator.js
+++ src/estimator.js
@@ -6,7 +6,8 @@ import { deriveImpact } from './functions';
 const covid19ImpactEstimator = (data) => ({
   data,
   impact: deriveImpact(data),
-  severeImpact: deriveImpact(data, 5),
+  // eslint-disable-next-line comma-dangle
+  severeImpact: deriveImpact(data, 5)
 });
 
 export default covid19ImpactEstimator;
