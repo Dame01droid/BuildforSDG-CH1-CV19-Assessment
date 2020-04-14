80bef6e31db30e87072149b0b4d69b6bd4139880
Damilola Alabi <47612686+Dame01droid@users.noreply.github.com>
2020-04-14 16:47:39 +0100

    estimor  refactoring

--- src/estimator.js
+++ src/estimator.js
@@ -1,13 +1,8 @@
-/* eslint-disable import/no-unresolved */
-// eslint-disable-next-line import/no-unresolved
-// eslint-disable-next-line import/extensions
-const deriveimpact = require('./functions');
 import * as functions from './functions';
 
 const covid19ImpactEstimator = (data) => ({
   data,
   impact: functions.deriveImpact(data),
-  // eslint-disable-next-line comma-dangle
   severeImpact: functions.deriveImpact(data, 5)
 });
 
