3391e316c045c8cb79ecdb52a77426c1397d3832
Damilola Alabi <47612686+Dame01droid@users.noreply.github.com>
2020-04-14 02:09:59 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,19 @@
-module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-    jest: true
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {
-    "comma-dangle": ["error", "never"],
-    "no-param-reassign": ["error", { "props": false }]
-  },
-}; 
+export const env = {
+  browser: true,
+  es6: true,
+  node: true,
+  jest: true
+};
+export const extends = ['airbnb-base'];
+export const globals = {
+  Atomics: 'readonly',
+  SharedArrayBuffer: 'readonly'
+};
+export const parserOptions = {
+  ecmaVersion: 2018,
+  sourceType: 'module'
+};
+export const rules = {
+  'comma-dangle': ['error', 'never'],
+  'no-param-reassign': ['error', { props: false }]
+};
